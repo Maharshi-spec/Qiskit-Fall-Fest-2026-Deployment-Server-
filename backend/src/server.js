42f5a9a61c88d6ce948e4f8fd7015425cd6d3526
Dineshkartheek <dineshkartheekvajrapu@gmail.com>
2026-09-07 11:51:28 +0530

    fixed the database error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,16 @@
 const app = require('./app')
 const { port } = require('./config/env')
+const { initializeDatabase } = require('./config/database-init')
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+const startServer = async () => {
+  await initializeDatabase()
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+  })
+}
+
+startServer().catch((error) => {
+  console.error('Unable to start server:', error)
+  process.exitCode = 1
 })
